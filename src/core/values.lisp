c4c95283dbcfc413c58a81a41d59439f9594ab24
porky11 <porky11@Linux.Speedport_W_921V_1_39_000>
2016-07-27 22:48:32 +0200

    values.lisp

--- src/core/values.lisp
+++ src/core/values.lisp
@@ -10,7 +10,7 @@
 (defun dump-value (val)
   (finish-output *error-output*)
   (%dump-value val))
-(defcfun* "LLVMPrintTypeToString" :string (m value))
+(defcfun* "LLVMPrintValueToString" :string (m value))
 
 (defcfun* "LLVMPrintValueToString" :string (val value))
 
